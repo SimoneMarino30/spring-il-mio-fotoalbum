7eef258a9789d3d38c19658b014925946e90bdb6
Simone Marino <simo.marino@outlook.it>
2023-07-06 16:51:42 +0200

    Added Update methods + messages in index view

--- src/main/java/org/lessons/springilmiofotoalbum/controller/PhotoController.java
+++ src/main/java/org/lessons/springilmiofotoalbum/controller/PhotoController.java
@@ -1,6 +1,8 @@
 package org.lessons.springilmiofotoalbum.controller;
 
 import jakarta.validation.Valid;
+import org.lessons.springilmiofotoalbum.messages.AlertMessage;
+import org.lessons.springilmiofotoalbum.messages.AlertMessageType;
 import org.lessons.springilmiofotoalbum.model.Photo;
 import org.lessons.springilmiofotoalbum.repository.CategoryRepository;
 import org.lessons.springilmiofotoalbum.repository.PhotoRepository;
@@ -13,6 +15,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -54,34 +57,21 @@ public class PhotoController {
         return "/photos/show";
     }
 
-    // metodo per selezionare photo da Db o tirare exception
-    private Photo getPhotoById(Integer id) {
-        // verificare se esiste una pizza con quell' id
-        Optional<Photo> result = photoRepository.findById(id);
-        // se non esiste, ritorno un http 404
-        if (result.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Photo with id " + id + " not found");
-            // eccezione 'custom' per richieste http
-        }
-        return result.get();
-    }
-
     /* *
     METODI PER LA CREATE
     * */
-    // controller che restituisce la pagina con form di creazione del nuovo book
-    // controller che restituisce la pagina con form di creazione del nuovo book
+    // controller che restituisce la pagina con form di creazione della nuova photo
     @GetMapping("/create")
     public String create(Model model) {
         // aggiungo al model l'attributo photo contenente una Photo vuota
         model.addAttribute("photo", new Photo());
-        return "/photos/edit"; // template con form di creazione di un book
+        return "/photos/edit"; // template con form di creazione di una photo
     }
 
     // controller che gestisce la post del form coi dati della photo
     @PostMapping("/create")
-    public String store(@Valid @ModelAttribute("photo") Photo formPhoto, BindingResult bindingResult) {
-        // i dati del book sono dentro all'oggetto formBook
+    public String store(@Valid @ModelAttribute("photo") Photo formPhoto, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        // i dati del book sono dentro all'oggetto formPhoto
 
         // verifico se l'isbn è univoco
         if (!isUniqueTitle(formPhoto)) {
@@ -92,22 +82,81 @@ public class PhotoController {
         // verifico se in validazione ci sono stati errori
         if (bindingResult.hasErrors()) {
             // ci sono stati errori
-            return "/photos/edit"; // ritorno il template del form ma col book precaricato
+            return "/photos/edit"; // ritorno il template del form ma con la photo precaricata
         }
 
         // setto il timestamp di creazione
         formPhoto.setCreatedAt(LocalDateTime.now());
-        // persisto formBook su database
+        // persisto formPhoto su database
         // il metodo save fa una create sql se l'oggetto con quella PK non esiste, altrimenti fa update
         photoRepository.save(formPhoto);
-
+        redirectAttributes.addFlashAttribute("message",
+                new AlertMessage(AlertMessageType.SUCCESS, "Photo created!"));
         // se tutto va a buon fine rimando alla lista dei books
         return "redirect:/photos";
     }
+
+    /* *
+    METODI PER UPDATE
+    * */
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Photo photo = getPhotoById(id);
+        // recupero i dati di quella photo da database
+        // aggiungo la photo al model
+        model.addAttribute("photo", photo);
+        // restituisco il template con il form di edit
+        return "/photos/edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String doEdit(
+            @PathVariable Integer id,
+            @Valid @ModelAttribute("photo") Photo formPhoto,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes
+    ) {
+        // cerco il book per id
+        Photo photoToEdit = getPhotoById(id); // vecchia versione della photo
+        // nuova versione della photo è formPhoto
+        // valido formPhoto
+        // se il vecchio title e quello nuovo sono diversi e quello nuovo è già presente su database allora errore
+        if (!photoToEdit.getTitle().equals(formPhoto.getTitle()) && !isUniqueTitle(formPhoto)) {
+            bindingResult.addError(new FieldError("book", "isbn", formPhoto.getTitle(), false, null, null,
+                    "Title must be unique"));
+        }
+        if (bindingResult.hasErrors()) {
+            // se ci sono errori ritorno il template col form
+            return "/photos/edit";
+        }
+
+        // trasferisco su formBook tutti i valori dei campi che non sono presenti nel form (altrimenti li perdo)
+        formPhoto.setId(photoToEdit.getId());
+        formPhoto.setCreatedAt(photoToEdit.getCreatedAt());
+        // salvo i dati
+        photoRepository.save(formPhoto);
+        redirectAttributes.addFlashAttribute("message",
+                new AlertMessage(AlertMessageType.SUCCESS, "Photo updated!"));
+        return "redirect:/photos";
+    }
+
+    // METODI CUSTOM
 
     // metodo per verificare se su database c'è già una con lo stesso title della photo passata come parametro
     private boolean isUniqueTitle(Photo formPhoto) {
         List<Photo> result = photoRepository.findByTitleContainingIgnoreCase(formPhoto.getTitle());
         return result.isEmpty();
+    }
+
+    // metodo per selezionare photo da Db o tirare exception
+    private Photo getPhotoById(Integer id) {
+        // verificare se esiste una pizza con quell' id
+        Optional<Photo> result = photoRepository.findById(id);
+        // se non esiste, ritorno un http 404
+        if (result.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Photo with id " + id + " not found");
+            // eccezione 'custom' per richieste http
+        }
+        return result.get();
     }
 }
