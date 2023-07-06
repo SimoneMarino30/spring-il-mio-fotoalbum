5e318b158702cc2a596f6d0d8d8d0ba5aeb6001f
Simone Marino <simo.marino@outlook.it>
2023-07-06 14:53:26 +0200

    Show view + Controller passes id through correctly

--- src/main/java/org/lessons/springilmiofotoalbum/controller/PhotoController.java
+++ src/main/java/org/lessons/springilmiofotoalbum/controller/PhotoController.java
@@ -5,13 +5,17 @@ import org.lessons.springilmiofotoalbum.repository.CategoryRepository;
 import org.lessons.springilmiofotoalbum.repository.PhotoRepository;
 import org.lessons.springilmiofotoalbum.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/photos")
@@ -23,22 +27,38 @@ public class PhotoController {
     @Autowired
     PhotoService photoService;
 
-    /* metodo che può ricevere opzionalmente un parametro da query string,
-  - se quel parametro c'è
-    dobbiamo filtrare la lista delle photos per quel parametro
-  - se quel parametro non c'è dobbiamo restituire la lista di tutti le photos
-  * */
     @GetMapping
     public String index(
             @RequestParam(name = "keyword", required = false) String searchString,
             Model model) { // Model è la mappa di attributi che il controller passa alla view
         List<Photo> photos = photoService.getFilteredPhotosByTitle(searchString);
 
-
         // passo la lista delle photos alla view
         model.addAttribute("photoList", photos);
         model.addAttribute("searchInput", searchString == null ? "" : searchString);
         // restituisco il nome del template della view
         return "/photos/index";
+    }
+
+    @GetMapping("/{id}")
+    public String detail(@PathVariable("id") Integer photoId, Model model) {
+
+        Photo photo = getPhotoById(photoId);
+        // passa la photo alla view
+        model.addAttribute("photo", photo);
+        // ritorna il nome del template della view
+        return "/photos/show";
+    }
+
+    // metodo per selezionare photo da Db o tirare exception
+    private Photo getPhotoById(Integer id) {
+        // verificare se esiste una pizza con quell' id
+        Optional<Photo> result = photoRepository.findById(id);
+        // se non esiste, ritorno un http 404
+        if (result.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Photo with id " + id + " not found");
+            // eccezione 'custom' per richieste http
+        }
+        return result.get();
     }
 }
--- src/main/java/org/lessons/springilmiofotoalbum/service/PhotoService.java
+++ src/main/java/org/lessons/springilmiofotoalbum/service/PhotoService.java
@@ -3,7 +3,9 @@ package org.lessons.springilmiofotoalbum.service;
 import org.lessons.springilmiofotoalbum.model.Photo;
 import org.lessons.springilmiofotoalbum.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,17 +15,17 @@ public class PhotoService {
     @Autowired
     PhotoRepository photoRepository;
 
+    // metodo che restituisce la lista di tutti i libri filtrata o no a seconda del parametro
+
     public List<Photo> getFilteredPhotosByTitle (String searchString) {
         if (searchString == null || searchString.isBlank()) {
             // se non ho il parametro searchString faccio la query generica
-            // recupero la lista di libri dal database
-            return photoRepository.findAll();
+            // recupero la lista delle foto dal database
+        return photoRepository.findAll();
         } else {
             // se ho il parametro searchString faccio la query con filtro
-            // books = bookRepository.findByTitle(searchString);
             return photoRepository.findByTitleContainingIgnoreCase(
                     searchString);
         }
     }
-
 }
