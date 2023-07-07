20fad8bfdbed2505b43c9196e4f2b4c8f2909620
Simone Marino <simo.marino@outlook.it>
2023-07-07 09:47:20 +0200

    Added categories on Photo templates + Categories templates themselves

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,9 +3,11 @@ INSERT INTO photos (title, description, url_pic, visible, created_at) VALUES('Ma
 INSERT INTO photos (title, description, url_pic, visible, created_at) VALUES('Trentino', 'Embark on a culinary journey to Trentino with this enticing pizza: Agerola fior di latte, smoky speck, creamy buffalo ricotta, and delicate zucchini blossoms. A delightful blend of flavors inspired by the enchanting region of Trentino!', 'https://res.cloudinary.com/glovoapp/w_600,f_auto,q_auto:low/Products/mqg2xcbdupwnuhiw7a5l', true, '2023-07-06 15:14');
 <!-- CATEGORIES
 INSERT INTO categories (name) VALUES('Pizze');
+INSERT INTO categories (name) VALUES('Paesaggi');
 <!-- TABELLA PONTE PHOTOS-CATEGORIES
 INSERT INTO photo_category (photo_id, category_id) VALUES(1,1);
 INSERT INTO photo_category (photo_id, category_id) VALUES(2,1);
+INSERT INTO photo_category (photo_id, category_id) VALUES(2,2);
 <!-- SECURITY
 INSERT INTO roles (id, name) VALUES(1,'ADMIN');
 INSERT INTO roles (id, name) VALUES(2,'USER');
